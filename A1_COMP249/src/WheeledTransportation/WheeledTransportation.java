1e94c0ba9544b703391b76e5f2ec19e9fdb6cf99
miiyao7 <120287233+miiyao7@users.noreply.github.com>
2023-10-02 18:15:17 -0400

    Update WheeledTransportation.java

--- A1_COMP249/src/WheeledTransportation/WheeledTransportation.java
+++ A1_COMP249/src/WheeledTransportation/WheeledTransportation.java
@@ -1,38 +1,39 @@
 package WheeledTransportation;
 
+
 public class WheeledTransportation {
 	private int nbOfWheels;
 	private double maxSpeed;
-	private long serialNumber;
+	private static long serialNb = 1;
 	int nbCreated;
 // default constructor	
 	public WheeledTransportation() {
 		nbOfWheels = 0;
 		maxSpeed = 0;
-		serialNumber = 0;
+		serialNb = getNextSerialNumber();
 		nbCreated++;
 	}
 // parameterized constructor	
 	public WheeledTransportation(int inNbOfWheels, double inMaxSpeed, long inSerialNumber) {
-		nbOfWheels = nbOfWheels;
-		maxSpeed = maxSpeed;
-		serialNumber = inSerialNumber;
+		nbOfWheels = inNbOfWheels;
+		maxSpeed = inMaxSpeed;
+		serialNb = getNextSerialNumber();
 		nbCreated++;
 	}
 // copy constructor
 	public WheeledTransportation(WheeledTransportation anotherWheeledTransportation) {
 		this.nbOfWheels = anotherWheeledTransportation.nbOfWheels;
 		this.maxSpeed = anotherWheeledTransportation.maxSpeed;
-		this.serialNumber = anotherWheeledTransportation.serialNumber;
+		this.serialNb = getNextSerialNumber();
 		nbCreated++;
 	}
 // custom method	
-	public long getNextSerialNumber() {
-		return serialNumber++;
+	public static long getNextSerialNumber() {
+		return serialNb;
 	}
 // override methods
 	public String toString() {
-		return "This wheeled transportation - serial #" + serialNumber + " - has " + nbOfWheels + ", has a maximum speed of " + maxSpeed + " km/hr. It has " + nbCreated + "vehicles and its starting and destinations stations are Montreal and Toronto";
+		return "This " + this.getClass().getSimpleName() + " - serial #" + serialNb + " - has " + nbOfWheels + ", has a maximum speed of " + maxSpeed + " km/hr. It has " + nbCreated + " vehicles.";
 	}
 	
 	
