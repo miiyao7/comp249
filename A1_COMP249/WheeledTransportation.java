ce7fc5a50fb3a4434991f81cf97d2bf80f17c531
miiyao7 <120287233+miiyao7@users.noreply.github.com>
2023-10-04 17:05:45 -0400

    Update WheeledTransportation.java

--- A1_COMP249/WheeledTransportation.java
+++ A1_COMP249/WheeledTransportation.java
@@ -4,27 +4,23 @@ public class WheeledTransportation {
 	private int nbOfWheels;
 	private double maxSpeed;
 	private static long serialNb = 1;
-	private static int nbCreated;
 // default constructor	
 	public WheeledTransportation() {
 		nbOfWheels = 0;
 		maxSpeed = 0;
 		serialNb = 1;
-		nbCreated++;
 	}
 // parameterized constructor	
 	public WheeledTransportation(int inNbOfWheels, double inMaxSpeed) {
 		nbOfWheels = inNbOfWheels;
 		maxSpeed = inMaxSpeed;
 		serialNb = 1;
-		nbCreated++;
 	}
 // copy constructor
 	public WheeledTransportation(WheeledTransportation anotherWheeledTransportation) {
 		this.nbOfWheels = anotherWheeledTransportation.nbOfWheels;
 		this.maxSpeed = anotherWheeledTransportation.maxSpeed;
 		serialNb = getNextSerialNumber();
-		nbCreated++;
 	}
 // getters method
 	public int getNbOfWheels() {
@@ -36,9 +32,7 @@ public class WheeledTransportation {
 	public long getSerialNb() {
 		return serialNb;
 	}
-	public int getNbCreated() {
-		return nbCreated;
-	}
+
 // setter method
 	public void setNbOfWheels(int inNbOfWheels) {
 		nbOfWheels = inNbOfWheels;
@@ -49,16 +43,14 @@ public class WheeledTransportation {
 	public void setSerialNb(int inSerialNb) {
 		serialNb = inSerialNb;
 	}
-	public void setNbCreated(int inNbCreated) {
-		nbCreated = inNbCreated;
-	}
+
 // custom method	
 	public long getNextSerialNumber() {
 		return serialNb++;
 	}
 // override methods
 	public String toString() {
-		return "This " + this.getClass().getSimpleName() + " - serial #" + getNextSerialNumber() + " - has " + nbOfWheels + ", has a maximum speed of " + maxSpeed + " km/hr. It has " + nbCreated + " vehicles";
+		return "This " + this.getClass().getSimpleName() + " - serial #" + getNextSerialNumber() + " - has " + nbOfWheels + " wheels, has a maximum speed of " + maxSpeed + " km/hr.";
 	}
 	public boolean equals(WheeledTransportation object) {
 		if(object == null || getClass() != object.getClass()) {
@@ -70,6 +62,5 @@ public class WheeledTransportation {
 		else 
 			return false;	 
 	}
-	
 	
 }
